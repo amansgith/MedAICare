e1cfc7e027a371063022c6e103351f7a06b3c039
Aman Khan <amankhancompq2002@gmail.com>
2023-10-08 19:59:28 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -103,68 +103,6 @@ def predict():
             return jsonify({"error": "Error processing the image: " + str(e)})
     return jsonify({"result": "Please upload an image for prediction"})
 
-
-
-# @app.route('/predict', methods=['GET','POST'])
-# @cross_origin()
-# def predict_pneumonia_route():
-#     if request.method == 'POST':
-#         if 'file' not in request.files:
-#             return jsonify({"error": "Please try again. The Image doesn't exist"})
-#         file = request.files.get('file')
-
-#         if not file:
-#             return
-
-#         try:
-#             img_bytes = file.read()
-#             img = detect_pneumonia(img_bytes)
-#             predictor = predict_pneumonia(img)
-#             result = 'Pneumonia Detected' if predictor else 'Pneumonia not detected'
-#             return jsonify({"result": result})
-#         except Exception as e:
-#             return jsonify({"error": "Error processing the image: " + str(e)})
-
-
-# # Set your OpenAI API key here
-# openai.api_key = "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"
-
-# @app.route('/chatbot', methods=['GET','POST'])
-# @cross_origin() # add cross_origin decorator
-# def chatbot():
-#     if request.method == 'POST':
-#         if not request.is_json:
-#             return jsonify({"error": "Please send JSON data"}) # use jsonify instead of render_template
-#         data = request.get_json()
-#         query = data.get('query')
-#         if not query:
-#             return jsonify({"error": "Please send a query"}) # use jsonify instead of render_template
-
-#         try:
-#             # Make a request to the OpenAI API with the query as prompt
-#             response = openai.Completion.create(
-#                 engine="davinci", # use davinci engine for best results
-#                 prompt=query, # use query as prompt
-#                 max_tokens=150, # limit the number of tokens in response
-#                 temperature=0.8, # adjust this parameter to control the randomness of the chatbot
-#                 frequency_penalty=0.2, # adjust this parameter to control the repetition of the chatbot
-#                 presence_penalty=0.2, # adjust this parameter to control the consistency of the chatbot
-#                 stop=["\n"] # stop the response at a newline character
-#             )
-#             response = response.choices[0].text # get the chatbot's reply from the response
-#             print(response)
-#             return jsonify({"response": response}) # use jsonify instead of render_template and return the reply
-#         except Exception as e:
-#             print(e)
-#             print("Error using the OpenAI API:", str(e))
-#             return jsonify({"error": "Error using the OpenAI API: " + str(e)}) # use jsonify instead of render_template
-#     return jsonify({"response": "Please send a POST request with a query"}) # use jsonify instead of render_template
-
-
-
-# with app.app_context():
-#     db.create_all()
-
 if __name__ == '__main__':
     app.run(debug=True)
 
